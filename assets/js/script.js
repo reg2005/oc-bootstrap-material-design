dfc8e700699d1ef58c693e020e1b4d61704a4036
reg2005 <cmp08@ya.ru>
2016-06-06 08:38:06 +0000

    Bugfix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,3 @@
 $(function () {
     $.material.init();
-    $(".shor").noUiSlider({
-        start: 40,
-        connect: "lower",
-        range: {
-            min: 0,
-            max: 100
-        }
-    });
-
-    $(".svert").noUiSlider({
-        orientation: "vertical",
-        start: 40,
-        connect: "lower",
-        range: {
-            min: 0,
-            max: 100
-        }
-    });
 });
